947c2dd627c16182f0bf973fde6bd44ff52d7c9c
Kylehaynessmart <kyle.smart100@gmail.com>
2024-11-06 17:45:17 +0200

    Form Design

--- PRG282_Project/Form1.Designer.cs
+++ PRG282_Project/Form1.Designer.cs
@@ -28,13 +28,224 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtStudentID = new System.Windows.Forms.TextBox();
+            this.txtStudentSurname = new System.Windows.Forms.TextBox();
+            this.txtStudentName = new System.Windows.Forms.TextBox();
+            this.txtCourse = new System.Windows.Forms.TextBox();
+            this.BtnAddStudent = new System.Windows.Forms.Button();
+            this.btnViewStudents = new System.Windows.Forms.Button();
+            this.BtnUpdateStudent = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.BtnSummary = new System.Windows.Forms.Button();
+            this.BtnClear = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(300, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Student Records";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtCourse);
+            this.groupBox1.Controls.Add(this.txtStudentName);
+            this.groupBox1.Controls.Add(this.txtStudentSurname);
+            this.groupBox1.Controls.Add(this.txtStudentID);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(133, 97);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(503, 207);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Details";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(73, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 16);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Student Name:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(73, 64);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(109, 16);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Student Surname";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(73, 21);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(74, 16);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Student ID: ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(73, 155);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 16);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "Course: ";
+            // 
+            // txtStudentID
+            // 
+            this.txtStudentID.Location = new System.Drawing.Point(200, 15);
+            this.txtStudentID.Name = "txtStudentID";
+            this.txtStudentID.Size = new System.Drawing.Size(100, 22);
+            this.txtStudentID.TabIndex = 4;
+            // 
+            // txtStudentSurname
+            // 
+            this.txtStudentSurname.Location = new System.Drawing.Point(200, 64);
+            this.txtStudentSurname.Name = "txtStudentSurname";
+            this.txtStudentSurname.Size = new System.Drawing.Size(100, 22);
+            this.txtStudentSurname.TabIndex = 5;
+            // 
+            // txtStudentName
+            // 
+            this.txtStudentName.Location = new System.Drawing.Point(200, 109);
+            this.txtStudentName.Name = "txtStudentName";
+            this.txtStudentName.Size = new System.Drawing.Size(100, 22);
+            this.txtStudentName.TabIndex = 6;
+            // 
+            // txtCourse
+            // 
+            this.txtCourse.Location = new System.Drawing.Point(200, 155);
+            this.txtCourse.Name = "txtCourse";
+            this.txtCourse.Size = new System.Drawing.Size(100, 22);
+            this.txtCourse.TabIndex = 7;
+            // 
+            // BtnAddStudent
+            // 
+            this.BtnAddStudent.Location = new System.Drawing.Point(12, 333);
+            this.BtnAddStudent.Name = "BtnAddStudent";
+            this.BtnAddStudent.Size = new System.Drawing.Size(115, 47);
+            this.BtnAddStudent.TabIndex = 3;
+            this.BtnAddStudent.Text = "Add Student";
+            this.BtnAddStudent.UseVisualStyleBackColor = true;
+            // 
+            // btnViewStudents
+            // 
+            this.btnViewStudents.Location = new System.Drawing.Point(147, 333);
+            this.btnViewStudents.Name = "btnViewStudents";
+            this.btnViewStudents.Size = new System.Drawing.Size(115, 47);
+            this.btnViewStudents.TabIndex = 4;
+            this.btnViewStudents.Text = "View Student Database";
+            this.btnViewStudents.UseVisualStyleBackColor = true;
+            // 
+            // BtnUpdateStudent
+            // 
+            this.BtnUpdateStudent.Location = new System.Drawing.Point(280, 333);
+            this.BtnUpdateStudent.Name = "BtnUpdateStudent";
+            this.BtnUpdateStudent.Size = new System.Drawing.Size(115, 47);
+            this.BtnUpdateStudent.TabIndex = 5;
+            this.BtnUpdateStudent.Text = "Update Students Information";
+            this.BtnUpdateStudent.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(411, 333);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(115, 47);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Delete a Student";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // BtnSummary
+            // 
+            this.BtnSummary.Location = new System.Drawing.Point(547, 333);
+            this.BtnSummary.Name = "BtnSummary";
+            this.BtnSummary.Size = new System.Drawing.Size(115, 47);
+            this.BtnSummary.TabIndex = 7;
+            this.BtnSummary.Text = "Generate Summary Report";
+            this.BtnSummary.UseVisualStyleBackColor = true;
+            // 
+            // BtnClear
+            // 
+            this.BtnClear.Location = new System.Drawing.Point(673, 333);
+            this.BtnClear.Name = "BtnClear";
+            this.BtnClear.Size = new System.Drawing.Size(115, 47);
+            this.BtnClear.TabIndex = 8;
+            this.BtnClear.Text = "Clear";
+            this.BtnClear.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BtnClear);
+            this.Controls.Add(this.BtnSummary);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.BtnUpdateStudent);
+            this.Controls.Add(this.btnViewStudents);
+            this.Controls.Add(this.BtnAddStudent);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCourse;
+        private System.Windows.Forms.TextBox txtStudentName;
+        private System.Windows.Forms.TextBox txtStudentSurname;
+        private System.Windows.Forms.TextBox txtStudentID;
+        private System.Windows.Forms.Button BtnAddStudent;
+        private System.Windows.Forms.Button btnViewStudents;
+        private System.Windows.Forms.Button BtnUpdateStudent;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button BtnSummary;
+        private System.Windows.Forms.Button BtnClear;
     }
 }
 
--- PRG282_Project/Form1.cs
+++ PRG282_Project/Form1.cs
@@ -16,5 +16,20 @@ namespace PRG282_Project
         {
             InitializeComponent();
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
