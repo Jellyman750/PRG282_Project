4a1dbd0cf771ce67060dff91dc3b07a0f1dc8348
Jan Frederik Cronje Saayman <577862@student.belgiumcampus.ac.za>
2024-11-07 14:23:57 +0200

    Delete function fixed

--- PRG282_Project/DeleteForm.cs
+++ PRG282_Project/DeleteForm.cs
@@ -8,11 +8,14 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using System.Data.SqlClient;
 
 namespace PRG282_Project
 {
     public partial class DeleteForm : Form
     {
+        SqlConnection conn = new SqlConnection("Server=JANNIE\\SQLEXPRESS; Initial Catalog=PRG282_Project; Integrated Security=true");
+
         DataHandler handler = new DataHandler();
         Student student = new Student();
 
@@ -28,9 +31,12 @@ namespace PRG282_Project
 
         private void button1_Click(object sender, EventArgs e)
         {
-            student.StudentID = int.Parse(textBox1.Text);
-           
-            handler.delete(student.StudentID);//We have to delete the row,and not just delete the ID from the Database,Kyk of of datagridviewRow kan gebruiik
+            student.StudentID = int.Parse((textBox1.Text));
+            student.Name = (textBox2.Text);
+            student.Age = int.Parse((textBox3.Text));
+            student.Course = textBox4.Text;
+
+            handler.delete(student.StudentID, student.Name, student.Age, student.Course);//We have to delete the row,and not just delete the ID from the Database,Kyk of of datagridviewRow kan gebruiik
             string filepath = "Delete.txt";
             handler.DeleteList.Add(new Student(student.StudentID, student.Name, student.Age, student.Course));
 
@@ -48,7 +54,25 @@ namespace PRG282_Project
 
         private void DeleteForm_Load(object sender, EventArgs e)
         {
+            string queryStudents = @"SELECT * FROM Students";
 
+            SqlDataAdapter studentAdapter = new SqlDataAdapter(queryStudents, conn);
+
+            DataSet ds = new DataSet();
+
+            studentAdapter.Fill(ds, "Students");
+
+            dataGridView1.DataSource = ds.Tables[0];
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
+
+            textBox1.Text = row.Cells[0].Value.ToString();
+            textBox2.Text = row.Cells[1].Value.ToString();
+            textBox3.Text = row.Cells[2].Value.ToString();
+            textBox4.Text = row.Cells[3].Value.ToString();
         }
     }
 }
